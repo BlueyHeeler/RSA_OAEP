8950bfe21ed5ba96292d9b5fb073a4273ee61b0c
BlueyHeeler <tairaerick@gmail.com>
2025-01-27 00:09:42 -0300

    Funções implementadas adicionadas.

--- src/funcoes.py
+++ src/funcoes.py
@@ -1,1 +1,55 @@
-cat >> implementacoes.py
+def decompondo(n):
+    """Decompõe n-1 como 2^k * m, onde n é ímpar."""
+    pass
+
+def miller_rabin(n, k=10):
+    """Algoritmo de Miller Rabin, determina de forma
+       probabilística se um número é primo."""
+    pass
+
+def mgf1(seed: int, length: int, hash_func=hashlib.sha1) -> bytes:
+    """Algoritmo MGF, similar a funções HASH, porém é possível
+       setar o tamanho da saída."""
+    pass
+
+def shift_left(shift_values, n):
+    pass
+
+def shift_right(shift_values, n):
+    pass
+
+def msb(n):
+    """Algoritmo que retorna o bit mais significativo """
+    pass
+
+def number_of_bits(n):
+    """Algoritmo que retorna a quantidade de bits ativos"""
+    pass
+
+def DB(pHash, mensagem):
+    """Sub etapa do OAEP"""
+   pass
+
+def prime_numbers():
+    """Função que retorna dois números primos de 1024 bits. """
+    pass
+
+def enc_oaep(mensagem):
+    """Encriptação com o OAEP"""
+    pass
+
+
+def dec_oaep(c):
+    """Decriptação do OAEP"""
+    pass
+
+def multiplicative_inverse(a, b):
+    """Função que retorna o inverso multiplicativo, "b * c ≅ 1 mod a", retorna o c """
+    pass
+
+def enc_rsa(n, e, m):
+    """Encriptação RSA"""
+    pass
+
+def dec_rsa(p, q, e, c):
+    """Decriptação RSA"""
